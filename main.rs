fa6fe2d8ef124f8389e74f64051d2b1ec452b16d
AlexPishchikov <cocacolaspriteman@gmail.com>
2021-02-12 08:52:59 +1000

    реализованы параметры -h и -H

--- main.rs
+++ main.rs
@@ -3,25 +3,29 @@ use std::fs;
 
 struct Config {
     v : bool,
+    h : bool,
+    H : bool,
 }
 
 fn main() {
     let mut config = Config {
         v : false,
+        h : true,
+        H : false,
     };
 
     let args : Vec<String> = env::args().skip(1).collect();
-    let mut params : Vec<String> = Vec::new();
+    let mut options : Vec<String> = Vec::new();
     let mut files : Vec<String> = Vec::new();
-    let mut substring : String = "".to_string();
+    let mut pattern : String = "".to_string();
     for arg in &args {
-        if substring == "" && arg.chars().next().unwrap().to_string() != "-" {
-            substring = arg.to_string();
+        if pattern == "" && arg.chars().next().unwrap().to_string() != "-" {
+            pattern = arg.to_string();
             continue;
         }
 
         if arg.chars().next().unwrap().to_string() == "-" {
-            params.push(arg.to_string());
+            options.push(arg.to_string());
             continue;
         }
 
@@ -31,9 +35,29 @@ fn main() {
         }
     }
 
-    for param in params {
-        if param == "-v" {
-            config.v = true;
+    if files.len() > 1 {
+        config.H = true;
+    }
+
+    for option in options {
+        match &*option {
+            "-v" => {
+                config.v = true;
+            },
+
+            "-h" => {
+                config.h = true;
+                config.H = false;
+            },
+
+            "-H" => {
+                config.H = true;
+                config.h = false;
+            },
+
+            _ => {
+                panic!("unknown option {}", option);
+            }
         }
     }
 
@@ -41,17 +65,20 @@ fn main() {
         let infile = fs::read_to_string(&file).expect("Something went wrong reading the file");
         let result = infile.split('\n');
         for string in result {
-            if is_match(&substring, &string, &config) {
-                let b : Vec<&str> = string.split(&substring).collect();
-                    for i in 0..b.len() - 1 {
-                        print!("{}\x1b[41m{}\x1b[0m", b[i], &substring);
-                    }
-                println!("{}", b[b.len() - 1]);
+            if is_match(&pattern, &string, &config) {
+                let split_string : Vec<&str> = string.split(&pattern).collect();
+                if config.H {
+                    print!("{}: ", file);
+                }
+                for i in 0..split_string.len() - 1 {
+                    print!("{}\x1b[41m{}\x1b[0m", split_string[i], &pattern);
+                }
+                println!("{}", split_string[split_string.len() - 1]);
             }
         }
     }
 }
 
-fn is_match(substring : &str, string : &str, config : &Config) -> bool {
-    return string.contains(substring) ^ config.v;
+fn is_match(pattern : &str, string : &str, config : &Config) -> bool {
+    return string.contains(pattern) ^ config.v;
 }
